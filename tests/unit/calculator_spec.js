478b3eaf09830884724f9859a992d0ffb3cd4580
Stephen <stephen_h369@live.co.uk>
2020-09-06 18:16:09 +0100

    unit test divide

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -29,4 +29,10 @@ describe('calculator', function () {
     assert.equal(15, calculator.runningTotal);
   })
 
+  it('can divide', function(){
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.equal(3, calculator.runningTotal);
+  })
+
 });
